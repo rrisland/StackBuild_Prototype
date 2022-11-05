0dffc258453a6ac6ea069691727c68e6a9376623
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-11-05 23:03:40 +0900

    ゴールカバーを同期

--- Assets/Scripts/Cover.cs
+++ Assets/Scripts/Cover.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Unity.Burst;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class Cover : MonoBehaviour
+public class Cover : NetworkBehaviour
 {
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private MeshCollider meshCollider;
@@ -19,20 +24,59 @@ public class Cover : MonoBehaviour
     private void Start()
     {
         if (meshRenderer is null || meshCollider is null) return;
-        
-        StartCoroutine(AppearanceTimer());
+        CuverCloseOpen(true);
     }
-    
-    private IEnumerator AppearanceTimer()
+
+    public override void OnNetworkSpawn()
     {
+        ClientSyncedCuverCloseOpen(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+    private async UniTask ClientSyncedCuverCloseOpen(CancellationToken token)
+    {
+        if (!IsOwner)
+            return;
+
         while (true)
         {
-            meshRenderer.enabled = true;
-            meshCollider.enabled = true;
-            yield return new WaitForSeconds(appearanceTime);
-            meshRenderer.enabled = false;
-            meshCollider.enabled = false;
-            yield return new WaitForSeconds(intervalTime);
+            await UniTask.Delay(TimeSpan.FromSeconds(appearanceTime), cancellationToken: token);
+            CuverSyncedServerRpc(NetworkManager.LocalTime.Time);
+            await UniTask.Delay(TimeSpan.FromSeconds(intervalTime), cancellationToken: token);
+            CuverSyncedServerRpc(NetworkManager.LocalTime.Time);
         }
     }
+    
+    public void CuverCloseOpen(bool isClose)
+    {
+        meshRenderer.enabled = isClose;
+        meshCollider.enabled = isClose;
+    }
+
+    private async UniTask WaitAndCuverOpenClose(float timeToWait, CancellationToken token)
+    {
+        if (timeToWait > 0)
+            await UniTask.Delay(TimeSpan.FromSeconds(timeToWait), cancellationToken: token);
+        
+        CuverCloseOpen(!meshRenderer.enabled);
+        //Debug.LogError("Cuver: " + (meshRenderer.enabled ? "閉じてる" : "空いてる") + $"\nTime: {NetworkManager.LocalTime.Time - timeToWait}");
+    }
+
+    [ServerRpc]
+    private void CuverSyncedServerRpc(double time)
+    {
+        CuverSyncedClientRpc(time);
+        var timeToWait = time - NetworkManager.ServerTime.Time;
+        WaitAndCuverOpenClose((float) timeToWait, this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+    [ClientRpc]
+    private void CuverSyncedClientRpc(double time)
+    {
+        if (IsOwner)
+            return;
+
+        var timeToWait = time - NetworkManager.ServerTime.Time;
+        WaitAndCuverOpenClose((float)timeToWait, this.GetCancellationTokenOnDestroy()).Forget();
+    }
+    
 }
